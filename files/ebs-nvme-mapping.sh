1eda01776cb96c5da5e0dc5a8bf773c1fe454b27
jalev <james.bach@transferwise.com>
2018-08-22 14:17:12 +0000

    Ansible-izes

--- files/ebs-nvme-mapping.sh
+++ files/ebs-nvme-mapping.sh
@@ -5,6 +5,6 @@ PATH="${PATH}:/usr/sbin"
 for blkdev in $( nvme list | awk '/^\/dev/ { print $1 }' ) ; do
   mapping=$(nvme id-ctrl --raw-binary "${blkdev}" | cut -c3073-3104 | tr -s ' ' | sed 's/ $//g')
   if [[ "/dev/${mapping}" == /dev/* ]]; then
-    ( test -b "${blkdev}" && test -L "${mapping}" ) || ln -s "${blkdev}" "/dev/${mapping}"
+    ( test -b "${blkdev}" && test -L "/dev/${mapping}" ) || ln -s "${blkdev}" "/dev/${mapping}"
   fi
 done

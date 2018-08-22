0e5c6eb24b2b1081c84647bd052ed733bfa650f0
James Bach <jalev@users.noreply.github.com>
2018-08-22 12:59:52 +0100

    Update ebs-nvme-mapping.sh
    Apparently they've updated NVME to now NOT INCLUDE THE DEV. WHY WOULD THEY DO THIS....

--- ebs-nvme-mapping.sh
+++ ebs-nvme-mapping.sh
@@ -4,7 +4,7 @@ PATH="${PATH}:/usr/sbin"
 
 for blkdev in $( nvme list | awk '/^\/dev/ { print $1 }' ) ; do
   mapping=$(nvme id-ctrl --raw-binary "${blkdev}" | cut -c3073-3104 | tr -s ' ' | sed 's/ $//g')
-  if [[ "${mapping}" == /dev/* ]]; then
-    ( test -b "${blkdev}" && test -L "${mapping}" ) || ln -s "${blkdev}" "${mapping}"
+  if [[ "/dev/${mapping}" == /dev/* ]]; then
+    ( test -b "${blkdev}" && test -L "${mapping}" ) || ln -s "${blkdev}" "/dev/${mapping}"
   fi
 done

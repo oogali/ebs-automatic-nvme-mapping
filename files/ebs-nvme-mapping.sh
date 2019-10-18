cbe92fce97a29d88436dfb2bd3f7afc3c2f457ca
Péter Sinóros-Szabó <sszp@users.noreply.github.com>
2019-10-18 13:24:30 +0200

    fix bug when nvme returns /dev/sda1 not just sda1 (#4)

--- files/ebs-nvme-mapping.sh
+++ files/ebs-nvme-mapping.sh
@@ -3,7 +3,7 @@
 PATH="${PATH}:/usr/sbin"
 
 for blkdev in $( nvme list | awk '/^\/dev/ { print $1 }' ) ; do
-  mapping=$(nvme id-ctrl --raw-binary "${blkdev}" | cut -c3073-3104 | tr -s ' ' | sed 's/ $//g')
+  mapping=$(nvme id-ctrl --raw-binary "${blkdev}" | cut -c3073-3104 | tr -s ' ' | sed 's/ $//g' | sed 's/dev//g' | sed 's/\///g')
   if [[ "/dev/${mapping}" == /dev/* ]]; then
     ( test -b "${blkdev}" && test -L "/dev/${mapping}" ) || ln -s "${blkdev}" "/dev/${mapping}"
   fi

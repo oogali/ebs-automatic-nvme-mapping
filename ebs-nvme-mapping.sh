4b655a2f7ae4da65af53d6109075c571583e432a
Jacob Appleton <jacob.appleton@orbis.com>
2019-11-20 15:31:15 -0800

    Invert if statement to throw errors early. Update README to include updated script. Based on PR by @brahman81 and feedback.

--- ebs-nvme-mapping.sh
+++ ebs-nvme-mapping.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
-# Inspired by https://github.com/oogali/ebs-automatic-nvme-mapping
-# Thanks Oogali!
-# Tested on Ubuntu 16.04
-# To be used with the below udev rule (/etc/udev/rules.d/999-aws-ebs-nvme.rules)
-# SUBSYSTEM=="block", KERNEL=="nvme[0-9]*n[0-9]*", ATTRS{model}=="Amazon Elastic Block Store", PROGRAM+="/usr/local/sbin/ebs-nvme-mapping.sh /dev/%k" SYMLINK+="%c"
+# To be used with the udev rule: /etc/udev/rules.d/999-aws-ebs-nvme.rules
 
-if [[ -x /usr/sbin/nvme ]] && [[ -b ${1} ]]; then
-  # capture 32 bytes at an offset of 3072 bytes from the raw-binary data
-  # not all block devices are extracted with /dev/ prefix
-  # use `xvd` prefix instead of `sd`
-  # remove all trailing space
-  nvme_link=$( \
-    /usr/sbin/nvme id-ctrl --raw-binary "${1}" | \
-    /usr/bin/cut -c3073-3104 | \
-    /bin/sed 's/^\/dev\///g'| \
-    /bin/sed 's/^sd/xvd/'| \
-    /usr/bin/tr -d '[:space:]' \
-  );
-  echo $nvme_link;
+if [[ ! -x /usr/sbin/nvme ]]; then
+  echo "ERROR: NVME tools not installed." >> /dev/stderr
+  exit 1
 fi
+
+if [[ ! -b ${1} ]]; then
+  echo "ERROR: cannot find block device ${1}" >> /dev/stderr
+  exit 1
+fi
+
+# capture 32 bytes at an offset of 3072 bytes from the raw-binary data
+# not all block devices are extracted with /dev/ prefix
+# use `xvd` prefix instead of `sd`
+# remove all trailing space
+nvme_link=$( \
+  /usr/sbin/nvme id-ctrl --raw-binary "${1}" | \
+  /usr/bin/cut -c3073-3104 | \
+  /bin/sed 's/^\/dev\///g'| \
+  /bin/sed 's/^sd/xvd/'| \
+  /usr/bin/tr -d '[:space:]' \
+);
+echo $nvme_link;

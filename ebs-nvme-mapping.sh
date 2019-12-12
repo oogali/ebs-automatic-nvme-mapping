19e5de86337cc4a6e9bd6f82006857fbece8364d
Ricardo Rosales <missingcharacter@gmail.com>
2019-12-12 17:07:05 -0600

    Checking for 2 mapping names
    - Limiting `nvme list` results to EBS only
    - Checking for both mappings scenarios (/dev/<something> and <something>

--- ebs-nvme-mapping.sh
+++ ebs-nvme-mapping.sh
@@ -2,9 +2,11 @@
 
 PATH="${PATH}:/usr/sbin"
 
-for blkdev in $( nvme list | awk '/^\/dev/ { print $1 }' ) ; do
+for blkdev in $( nvme list | grep 'Amazon Elastic Block Store              ' | awk '/^\/dev/ { print $1 }' ) ; do
   mapping=$(nvme id-ctrl --raw-binary "${blkdev}" | cut -c3073-3104 | tr -s ' ' | sed 's/ $//g')
   if [[ "${mapping}" == /dev/* ]]; then
     ( test -b "${blkdev}" && test -L "${mapping}" ) || ln -s "${blkdev}" "${mapping}"
+  elif [[ "/dev/${mapping}" == /dev/* ]]; then
+    ( test -b "${blkdev}" && test -L "/dev/${mapping}" ) || ln -s "${blkdev}" "/dev/${mapping}"
   fi
 done

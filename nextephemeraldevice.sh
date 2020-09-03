1a8da64ff5005ae76724ca5cda1c1a098b543f43
Ricardo Rosales <missingcharacter@gmail.com>
2020-09-03 17:24:30 -0500

    adding lock file to ephemeral script

--- nextephemeraldevice.sh
+++ nextephemeraldevice.sh
@@ -1,9 +1,27 @@
 #!/usr/bin/env bash
 # To be used with the udev rule: /etc/udev/rules.d/999-aws-ebs-nvme.rules
 
+# check if lock file exists
+script_name="$(basename $0)"
+pid_file="/tmp/${script_name}.lock"
+counter=0
+until [ $counter -eq 5 ] || [[ ! -e "${pid_file}" ]] ; do
+  sleep $(( counter++ ))
+done
+
+# create lock file if it does not exist
+if [[ -e "${pid_file}" ]]; then
+  echo "Lock file ${pid_file} still exists after counter ended" >&2
+  exit 1
+else
+  touch "${pid_file}"
+fi
+
 kern_name=${1}
 incr=0
 while [[ -e "/dev/ephemeral${incr}" ]] && [[ $(readlink "/dev/ephemeral${incr}") != "${kern_name}" ]]; do
   incr=$[$i+1]
 done
+# remove lock file
+rm "${pid_file}"
 echo "ephemeral${incr}"
